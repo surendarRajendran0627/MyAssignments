e10ab3340b0f33967dc9652ef906cd602333711c
surendarRajendran0627 <surendardefendar@gmail.com>
2026-06-25 06:28:26 +0100

    Add files via upload

---
+++ Day_2_assignment/LoopingStatement.js
@@ -0,0 +1,13 @@
+function printOddNumbers(number) {
+
+ 
+for (let index = 1; index <=25; index++) {
+    if (index %2 !==0) {
+     console.log("The Odd number is : ", index);
+        
+    }
+    
+}
+}    
+printOddNumbers(25)
+
---
+++ Day_2_assignment/NumberType.js
@@ -0,0 +1,35 @@
+function checkNumber(num) {
+ //  const getResult
+    if (num > 0) {
+       console.log(num, " it is a positive number");
+       
+    }else if (num < 0){
+        console.log(num, " it is a negative number");
+    }
+    else  {
+       let num = 0
+    console.log(num, " it is a Neutral number");
+    }
+
+}
+
+checkNumber(1)
+checkNumber(-1)
+checkNumber(0)
+
+function validateNumber(num){
+    let getResult
+      if (num > 0) {
+      getResult = console.log(num, " it is a positive number");
+       
+    }else if (num < 0){
+       getResult = console.log(num, " it is a negative number");
+    }
+    else  {
+       let num = 0
+  getResult =  console.log(num, " it is a Neutral number");
+    }
+}
+validateNumber(1)
+validateNumber(-1)
+validateNumber(0)
